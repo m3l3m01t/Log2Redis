d322a614cf83144295a3a24f960e1758d93cd7a1
Jiff Shen <Jiff.Shen@wdc.com>
2018-11-02 22:24:03 +0800

    update nuspec & dependencies

--- Log2Redis/Log2Redis.cs
+++ Log2Redis/Log2Redis.cs
@@ -1,5 +1,7 @@
 ﻿using log4net.Appender;
 using log4net.Core;
+using log4net.Layout;
+using log4net.Util;
 using System.IO;
 
 namespace Log2Redis
@@ -9,6 +11,8 @@ namespace Log2Redis
     /// </summary>
     public class RedisPubAppender : AppenderSkeleton
     {
+        private RedisLogger _logger;
+
         public int Port { get; set; } = 6379;
 
         public string Host { get; set; }
@@ -20,21 +24,28 @@ namespace Log2Redis
         /// </summary>
         protected override void Append(LoggingEvent evt)
         {
-            var logger = RedisLogger.Get(Host, Port, Topic, true);
-            if (logger != null)
+            if (_logger != null)
             {
                 var writer = new StringWriter();
                 Layout.Format(writer, evt);
 
-                logger.Log($"{Topic}/{evt.LoggerName}/{evt.Level}", writer.ToString());
+                var message = writer.ToString();
+
+                new PatternLayout(Topic).Format(writer, evt);
+
+                _logger.Log(writer.ToString(), message);
             }
         }
 
         protected override bool PreAppendCheck()
         {
-            if (Host == null || Topic == null)
-                return false;
+            if (_logger == null)
+            {
+                if (Host == null || Topic == null)
+                    return false;
 
+                _logger = RedisLogger.Get(Host, Port, Topic, true);
+            }
             return base.PreAppendCheck();
         }
 
